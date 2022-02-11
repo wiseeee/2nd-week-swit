cb3964d3be926b85539622f1058720b24f770322
minbr0ther <minbr0ther@naver.com>
2022-02-11 14:39:32 +0900

    feat: 멀티라인으로 입력 가능 메시지에서의 출력도 그대로 출력

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Input from 'components/Input/index';
 import MessageList from 'components/MessageList/index';
-import { useDispatch, useSelector } from 'react-redux';
-import { logIn } from 'reducers/reducer';
 import ChannelToolbar from 'components/ChannelToolbar';
 import * as S from 'styles/styled';
 
 function App() {
-  const name = useSelector((state) => state.logInReducer.user.name);
-  const dispatch = useDispatch();
-  dispatch(logIn('test'));
+  // const name = useSelector((state) => state.logInReducer.user.name);
+  // const dispatch = useDispatch();
+  // dispatch(logIn('test'));
 
-  useEffect(() => {
-    // const userInput = prompt('사용자 이름을 입력해주세요.');
-    // dispatch(logIn(userInput));
-    // alert(`반갑습니다 ${userInput}님 😀`);
-  }, []);
+  // useEffect(() => {
+  // const userInput = prompt('사용자 이름을 입력해주세요.');
+  // dispatch(logIn(userInput));
+  // alert(`반갑습니다 ${userInput}님 😀`);
+  // }, []);
 
   return (
     <S.Layout>
@@ -23,7 +21,6 @@ function App() {
         <ChannelToolbar />
         <MessageList />
         <Input />
-        <span> 리덕스 테스트 : {name} </span>
       </S.Main>
     </S.Layout>
   );
--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,15 +1,50 @@
-import React from 'react';
-import { faCamera } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
+// import { faPaperPlaneTop } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Wrapper, MessageInput } from './styled';
 
 function Input() {
+  const [message, setMessage] = useState('');
+
+  const onChange = (e) => {
+    e.preventDefault();
+
+    setMessage(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    alert(message);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter' && e.shiftKey) {
+      return;
+    }
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      onSubmit(e);
+    }
+  };
+
   return (
-    <div>
-      <input type="text" />
-      <button type="button">
-        <FontAwesomeIcon icon={faCamera} size="2x" />
-      </button>
-    </div>
+    <Wrapper>
+      <form onSubmit={onSubmit}>
+        <MessageInput
+          type="text"
+          placeholder="Enter message"
+          name="message"
+          onChange={onChange}
+          onKeyDown={handleKeyPress}
+        />
+        <button type="submit" disabled={!message}>
+          전송!
+          {/* <FontAwesomeIcon icon={['fas', 'coffee']} /> */}
+        </button>
+      </form>
+    </Wrapper>
   );
 }
 
