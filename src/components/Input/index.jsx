2989cd923ce5b9bfc2bd6b041d0b1bd0c81212a0
minbr0ther <minbr0ther@naver.com>
2022-02-11 12:54:15 +0900

    chore: fontAwesome 설치

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { faCamera } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Input() {
-  return <div>Input</div>;
+  return (
+    <div>
+      <input type="text" />
+      <button type="button">
+        <FontAwesomeIcon icon={faCamera} size="2x" />
+      </button>
+    </div>
+  );
 }
 
 export default Input;
