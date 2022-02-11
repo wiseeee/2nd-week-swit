a95e6d0e7e4057917e69efbf1bba27e931b934be
minbr0ther <minbr0ther@naver.com>
2022-02-11 10:45:20 +0900

    chore: style-reset 설치 및 적용

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,30 @@ import React, { useEffect } from 'react';
 import Input from 'components/Input/index';
 import MessageList from 'components/MessageList/index';
 import { useDispatch, useSelector } from 'react-redux';
-import { logIn } from 'reducers/reducer';
+// import { logIn } from 'reducers/reducer';
+import ChannelToolbar from 'components/ChannelToolbar';
+import * as S from 'styles/styled';
 
 function App() {
   const name = useSelector((state) => state.logInReducer.user.name);
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
   useEffect(() => {
-    const userInput = prompt('사용자 이름을 입력해주세요.');
-
-    dispatch(logIn(userInput));
-
-    alert(`반갑습니다 ${userInput}님 😀`);
+    // const userInput = prompt('사용자 이름을 입력해주세요.');
+    // dispatch(logIn(userInput));
+    // alert(`반갑습니다 ${userInput}님 😀`);
   }, []);
 
   return (
     <div className="App">
-      <MessageList />
-      <Input />
-      <span> 리덕스 테스트 : {name} </span>
+      <S.Layout>
+        <S.Main>
+          <ChannelToolbar />
+          <MessageList />
+          <Input />
+          <span> 리덕스 테스트 : {name} </span>
+        </S.Main>
+      </S.Layout>
     </div>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import GlobalStyle from 'styles/globalStyles';
 import App from './App';
 import store from './store';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+      <GlobalStyle />
       <App />
     </Provider>
   </React.StrictMode>,
